97b07981df79b11d1fef13da782c1d3616ac6ae1
ungb <ungb@github.com>
2017-08-17 14:21:34 -0700

    sdfe

--- TestWebAppProject/Startup.cs
+++ TestWebAppProject/Startup.cs
@@ -34,6 +34,15 @@ namespace TestWebAppProject
             services.AddMvc();
         }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        /// <param name="loggerFactory"></param>
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
