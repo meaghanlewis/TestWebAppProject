8cec2b8c2e48569a13abd64daddcdc66ae5106f2
githubtestuser <githubtestuser@outlook.com>
2017-10-23 18:32:21 -0700

    update home controller

--- TestWebAppProject/Controllers/HomeController.cs
+++ TestWebAppProject/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ namespace TestWebAppProject.Controllers
         {
             ViewData["Message"] = "Your application description page.";
             ViewData["Message"] = "Your application description page.";
+			ViewData["Message"] = "Your application description page.";
 
             return View();
         }
