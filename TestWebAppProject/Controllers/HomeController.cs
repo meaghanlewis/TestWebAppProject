3df10c5664a2f42099169c50e7ccaea89bd2751a
githubtestuser <githubtestuser@outlook.com>
2017-10-23 18:34:13 -0700

    update home controller

--- TestWebAppProject/Controllers/HomeController.cs
+++ TestWebAppProject/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace TestWebAppProject.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your contact page!";
 
             return View();
         }
