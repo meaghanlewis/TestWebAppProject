9c8ab27ce889291f3d2e7c07e45779dc45bea5b9
meaghanlewis <meaghanlewis@github.com>
2018-05-21 12:55:34 -0700

    update unit test

--- UnitTestProject3/UnitTestProject3/UnitTest1.cs
+++ UnitTestProject3/UnitTestProject3/UnitTest1.cs
@@ -6,13 +6,5 @@ namespace UnitTestProject3
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-        [TestMethod]
-        public void TestMethod2()
-        {
-        }
     }
 }
