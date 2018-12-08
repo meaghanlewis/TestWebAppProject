65cc8fb7546c2cbebcec997a78c8ef569aa0d08e
Meaghan Lewis <meaghanlewis@github.com>
2018-12-07 16:33:20 -0800

    add sample class

---
+++ SampleClass.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "asdf"
+        }
+    }
+}
