430f3bfeef9289fb9eba08c4e39f4c84a38e0a40
Meaghan Lewis <moneikmarie@gmail.com>
2020-09-22 20:34:35 -0700

    Update Program.cs

--- TestWebAppProject/Program.cs
+++ TestWebAppProject/Program.cs
@@ -11,15 +11,6 @@ namespace TestWebAppProject
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.Run();
         }
     }
 }
